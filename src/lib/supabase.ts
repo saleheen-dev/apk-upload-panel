3e7d054cfdcf42d49a2a67f397cf8ec1f579dc8c
saleheen-dev <saleheen.noor@gmail.com>
2025-02-05 21:46:57 +0500

    minor changes for supabase envs

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,19 +1,22 @@
 import { createClient } from "@supabase/supabase-js";
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL");
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl) {
+  throw new Error(
+    "Missing NEXT_PUBLIC_SUPABASE_URL - Add to your environment variables"
+  );
 }
 
-if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
-  throw new Error("Missing SUPABASE_SERVICE_ROLE_KEY");
+if (!supabaseKey) {
+  throw new Error(
+    "Missing SUPABASE_SERVICE_ROLE_KEY - Add to your environment variables"
+  );
 }
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
-  {
-    auth: {
-      persistSession: false,
-    },
-  }
-);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false,
+  },
+});
